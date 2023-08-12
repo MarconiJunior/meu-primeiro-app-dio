8e4e80dcaf01c2eea1219c89e55531f004f07dce
MarconiJunior <junior@brumafe.com>
2023-08-12 12:02:40 -0300

    feat : project finished

--- app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
@@ -1,14 +1,34 @@
 package br.com.igorbag.meuprimeiroappdio
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //O desafio sera criar uma valor dentro do string.xml
-        // E trocar o texto do xml e tornar internacional (Ingles, Espanhol, etc...)
+        translateText(findViewById(R.id.brazil), 1)
+        translateText(findViewById(R.id.eua), 2)
+        translateText(findViewById(R.id.esp), 3)
+
+    }
+
+    private fun translateText(button: Button, number: Int) {
+        val stringRes = when (number) {
+            1 -> getString(R.string.text_pt)
+            2 -> getString(R.string.text_en)
+            3 -> getString(R.string.text_es)
+            else -> getString(R.string.text_pt)
+        }
+
+        button.setOnClickListener {
+            findViewById<TextView>(R.id.textTranslate).text = stringRes
+        }
     }
 }
+
